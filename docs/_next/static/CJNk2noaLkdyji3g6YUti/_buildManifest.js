f07516bf6bc1c7b2c802c545ad732a2a405dc0de
Md Adil <md-adil@live.com>
2021-08-15 19:30:47 +0530

    scrolable

---
+++ docs/_next/static/CJNk2noaLkdyji3g6YUti/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-aa2379f1393a41e3c70e.js"],"/_error":["static/chunks/pages/_error-a0e21b9b223f827fe1f2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
